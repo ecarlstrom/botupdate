0fda9e990451d26b00cad7d14b34d459a12b255a
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-10 22:05:20 -0500

    updates ping command with timer

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,6 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`Pong!`);
+    message.channel.send('Ping?')
+    .then(message => {
+        message.edit(`Pong! (time elapsed: ${message.createdTimestamp - message.createdTimestamp}ms)`);
+    });
 };
