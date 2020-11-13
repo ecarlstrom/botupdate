581df9245f9408abf38acd7076d36a8b08ceeef8
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-13 03:38:24 -0500

    adds embed formatting to queue output

--- commands/queue.js
+++ commands/queue.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 exports.run = async (client, message, args, ops) => {
     let fetched = ops.active.get(message.guild.id);
     if(!fetched) {
@@ -7,10 +9,19 @@ exports.run = async (client, message, args, ops) => {
     let queue = fetched.queue;
     let current = queue[0];
     let result = `__**Now playing**__\n${current.songTitle} Requested by ${current.requester}\n\n`;
+    let single = queue.length === 1;
+
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`Now playing: **${current.songTitle}**`)
+        .setColor(0xDD2825)
+        .setFooter(`Requested by ${current.requester}`, current.requesterIcon)
+        .setDescription(`There ${single ? 'is' : 'are'} currently ${queue.length} song${single ? '' : 's'} in the queue.\n`)
+        //.setThumbnail
+        .setTimestamp()
 
     for(let i = 1; i < queue.length; i++) {
-        result += `${i}. **${queue[i].songTitle}**\n`
+        embed.addField(`🤠 ${queue[i].songTitle} 🤠`, `Requested by **${queue[i].requester}**`);
     }
 
-    message.channel.send(result);
+    message.channel.send({embed});
 }
