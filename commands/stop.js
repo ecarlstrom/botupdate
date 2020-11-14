d17155d1aa1d81e8b55f2e63c9989b05094e7fa4
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-14 04:06:20 -0500

    adds embed formatting for stop command

--- commands/stop.js
+++ commands/stop.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 exports.run = async (client, message, args, ops) => {
     const voiceChannel = message.member.voice.channel ? message.member.voice.channel : (message.guild.voice.connection ? message.guild.voice.connection.channel : null);
 
@@ -14,7 +16,10 @@ exports.run = async (client, message, args, ops) => {
      if(queue) {
          fetched.queue = [];
          fetched.dispatcher.end();
-         message.channel.send(`Canceling queue!`);
+         const embed = new Discord.MessageEmbed()
+            .setTitle(`Queue canceled!`)
+            .setColor(0xfdfe03)
+        message.channel.send({embed});
      }
     
 }
