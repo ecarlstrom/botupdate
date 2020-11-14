be3254906347ff8e83e3bae025f9fb46d66b26d3
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-13 22:28:44 -0500

    adds current volume display if no value passed in, formats result as an embed

--- commands/volume.js
+++ commands/volume.js
@@ -10,11 +10,18 @@ exports.run = (client, message, args, ops) => {
         return message.channel.send(`You must be in the same channel as the bot to do this!`);
     }
 
-    if(isNaN(args[0]) || args[0] < 0 || args[0] > 100) {
+    
+    if(!args[0]) {
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`Current volume is ${fetched.dispatcher.volume}%.`)
+        .setColor(0xfdfe03)
+        return message.channel.send({embed});
+    }
+
+    if(args[0] && (isNaN(args[0]) || args[0] < 0 || args[0] > 100)) {
         return message.channel.send(`Please enter a number between 0 and 100!`);
     }
 
-    
     fetched.dispatcher.setVolume(args[0]/100);
     const embed = new Discord.MessageEmbed()
         .setTitle(`Volume set to ${args[0]}%!`)
