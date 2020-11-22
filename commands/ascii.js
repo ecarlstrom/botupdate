0ea610ba0f4c12f3e167d19dfef660d0ddef6f15
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-22 18:42:16 -0500

    adds another crash prevention check to ascii command, this one to make sure that undefined arguments (like emoji inputs) trigger an error rather than a crash

--- commands/ascii.js
+++ commands/ascii.js
@@ -5,6 +5,10 @@ exports.run = (client, message, args, ops) => {
         return message.reply(`🤠 Please enter some text to format! 🤠`)
     }
 
+    if(args[0].length  === undefined) {
+        return message.reply(`🤠 Please use a valid format! 🤠`)
+    }
+
     console.log(args[0]);
     ascii.font(args.join(' '), 'Doom', function(err, rendered) {
         rendered = rendered.trimRight();
