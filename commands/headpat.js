47151da8a2d5100b0ebde0a4539f7dbbea575afc
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-26 19:05:07 -0500

    adds headpat command

---
+++ commands/headpat.js
@@ -0,0 +1,43 @@
+exports.run = (client, message) => {
+    // concatenate arguments to avoid the space issue?
+    
+    if(message.content.startsWith(process.env.prefix)) {
+        message.reply("<:headpat:637295953925111818>");
+
+        // for reaction
+        // const headpatEmoji = message.guild.emojis.find(emoji => emoji.name === "headpat")
+        // return message.react(headpatEmoji);
+    };
+
+    // can add something for mentioning other users, maybe in a new command?
+    // parse the username with a function, e.g.:
+
+    // function retrieveUser(mention) {
+    //     if(!mention) {
+    //         return;
+    //     }
+
+    //     if(mention.startsWith('@') && mention.endsWith('>')) {
+    //         mention = mention.slice(2, -1); // to get the ID
+
+    //         if(mention.startsWith('!')) {
+    //             mention = mention.slice(1);
+    //         }
+
+    //         return client.users.get(mention);
+    //     }
+    // }
+
+    // the user can be grabbed like so
+    // if(message.content.startsWith(process.env.prefix)) {
+    //     if(args[0]) {
+    //         const user = retrieveUser(args[0]);
+    //         // not adding an if(!user) idea here so the base command stays functional
+            // mention user here with headpat emoji
+    //     }
+
+        // reactions again
+        // const headpatEmoji = message.guild.emojis.find(emoji => emoji.name === "headpat")
+        // return message.react(headpatEmoji);
+    // }
+};
