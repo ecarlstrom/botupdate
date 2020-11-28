e9f79bc3b093f4f14007d40c28bf8c3f0539ea5e
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-27 21:27:35 -0500

    improves output formatting for listnames command so data is returned in name | emoji format

--- commands/listnames.js
+++ commands/listnames.js
@@ -4,7 +4,7 @@ exports.run = (client, message) => {
     if(message.content === (process.env.prefix + 'listnames')) {
         // this version includes the numerical ID of each custom emoji:
         // const emojiList = message.guild.emojis.map((e, x) => (x + ' = ' + e) + ' | ' + e.name).join('\n');
-        const emojiList = message.guild.emojis.cache.map((e) => (e) + '   |   ' + e.name).join('\n\n');
+        const emojiList = message.guild.emojis.cache.map((e) => (e.name) + '   |   ' + e.toString()).join('\n\n');
         for(let i = 0; i < emojiList.length; i += 2000) {
           const splitList = emojiList.substring(i, Math.min(emojiList.length, i + 2000));
           const embed = new Discord.MessageEmbed()
