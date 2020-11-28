cc71137b590e23e9ac4a65ea758bfc99f6b34bd8
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-27 21:19:58 -0500

    fixes list command with new discord.js syntax

--- commands/list.js
+++ commands/list.js
@@ -4,10 +4,10 @@ exports.run = (client, message) => {
     // return a list of the server's custom emojis
 
     if(message.content === (process.env.prefix + 'list')) {
-        const emojiList = message.guild.emojis.map(e => e.toString()).join('   ');
+        const emojiList = message.guild.emojis.cache.map(e => e.toString()).join('   ');
         for(let i = 0; i < emojiList.length; i += 2000) {
         const splitList = emojiList.substring(i, Math.min(emojiList.length, i + 2000));
-        const embed = new Discord.RichEmbed()
+        const embed = new Discord.MessageEmbed()
             .setTitle(`Emojis for '${message.guild.name}': `)
             .setColor(0x003366)
             .setDescription(splitList);
