52a3a66d9c565e0246842bef4f1acb9a5cfb8ad1
ecarlstrom <evan.carlstrom@gmail.com>
2020-11-27 21:17:36 -0500

    creates command for listing emojis

---
+++ commands/list.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message) => {
+    // return a list of the server's custom emojis
+
+    if(message.content === (process.env.prefix + 'list')) {
+        const emojiList = message.guild.emojis.map(e => e.toString()).join('   ');
+        for(let i = 0; i < emojiList.length; i += 2000) {
+        const splitList = emojiList.substring(i, Math.min(emojiList.length, i + 2000));
+        const embed = new Discord.RichEmbed()
+            .setTitle(`Emojis for '${message.guild.name}': `)
+            .setColor(0x003366)
+            .setDescription(splitList);
+        message.channel.send({embed});
+        }
+    }
+};
